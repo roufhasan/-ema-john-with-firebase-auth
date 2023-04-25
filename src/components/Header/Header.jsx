23f9ac9f8b3b36e052f2ca6ded84b7aa9f66ad5c
roufhasane <roufhasan5@gmail.comm>
2023-04-25 16:09:03 +0600

    auth context added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import './Header.css';
-import logo from '../../images/Logo.svg';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import "./Header.css";
+import logo from "../../images/Logo.svg";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../providers/Authprovider";
 
 const Header = () => {
-    return (
-        <nav className='header'>
-            <img src={logo} alt="" />
-            <div>
-                <Link to="/">Shop</Link>
-                <Link to="/orders">Orders</Link>
-                <Link to="/inventory">Inventory</Link>
-                <Link to="/login">Login</Link>
-            </div>
-        </nav>
-    );
+  const { user } = useContext(AuthContext);
+  return (
+    <nav className="header">
+      <img src={logo} alt="" />
+      <div>
+        <Link to="/">Shop</Link>
+        <Link to="/orders">Orders</Link>
+        <Link to="/inventory">Inventory</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Sign up</Link>
+        {user && <span>Welcome {user.email}</span>}
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMdgrXiU-v81UFWrUE0m2RW23Vkk7kWzk",
+  authDomain: "ema-john-with-firebase-a-868c3.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a-868c3",
+  storageBucket: "ema-john-with-firebase-a-868c3.appspot.com",
+  messagingSenderId: "236623914585",
+  appId: "1:236623914585:web:624aa06afb1f257ac43348",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
